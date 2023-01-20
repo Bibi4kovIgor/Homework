523481d55d99b9d85eb23aefa552c02d000a574d
Bibi4kovIgor <work.bibi4kov@gmail.com>
2023-01-20 19:36:50 +0200

    Homework2 Added sort array

--- src/main/java/edu/demo/Main.java
+++ src/main/java/edu/demo/Main.java
@@ -1,5 +1,45 @@
-package edu.demo;public class Main {
+package edu.demo;
+
+import java.util.Arrays;
+/**
+ *
+ * Main method
+ * Method is used to launch app
+ * */
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {4, 5, 7, 98, 21, 7, 36};
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.binarySearch(array, 98));
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.binarySearch(array, 98));
+        System.out.println(Arrays.toString(getAllMultipleElements(array, 2)));
+
     }
+
+
+    /**
+     * <b> Methods </b> returns all <i>multiple</i>  elements for input <u> array </u> by input value
+     * @param array input array
+     * @param n     multiple to element
+     * @return int[]
+     * */
+    public static int[] getAllMultipleElements(int[] array, int n) {
+        int[] result = new int[array.length];
+        int i = 0;
+        for (int element : array) {
+            if(element % n == 0) {
+                result[i] = element;
+                i++;
+            }
+        }
+        return result;
+    }
+
+    public static int[] sortArray(int[] array) {
+        Arrays.sort(array);
+        return array;
+    }
+
 }
